e97f2273301ddfbd642e8027c569ad678016c2cb
Bruce Becker <brucellino@protonmail.com>
2022-04-03 15:13:36 +0200

    feat(droplet): provision my personal key
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- README.md
+++ README.md
@@ -21,6 +21,7 @@ No modules.
 
 | Name | Type |
 |------|------|
+| [digitalocean_spaces_bucket.images](https://registry.terraform.io/providers/digitalocean/digitalocean/2.19.0/docs/resources/spaces_bucket) | resource |
 | [digitalocean_ssh_key.test_instance](https://registry.terraform.io/providers/digitalocean/digitalocean/2.19.0/docs/resources/ssh_key) | resource |
 | [digitalocean_vpc.vpc](https://registry.terraform.io/providers/digitalocean/digitalocean/2.19.0/docs/resources/vpc) | resource |
 | [vault_generic_secret.do_token](https://registry.terraform.io/providers/hashicorp/vault/3.4.0/docs/data-sources/generic_secret) | data source |
@@ -30,6 +31,7 @@ No modules.
 
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
+| <a name="input_region"></a> [region](#input\_region) | DO Region to deploy into | `string` | `"ams3"` | no |
 | <a name="input_ssh_key_name"></a> [ssh\_key\_name](#input\_ssh\_key\_name) | SSH Key for digital ocean droplets | `string` | `"test-instances"` | no |
 
 ## Outputs
--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "= 2.19.0"
     }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
     vault = {
       source  = "hashicorp/vault"
       version = "= 3.4.0"
@@ -27,6 +32,8 @@ variable "ssh_key_name" {
   default     = "test-instances"
 }
 
+
+
 data "vault_generic_secret" "do_token" {
   path = "digitalocean/tokens"
 }
@@ -35,6 +42,9 @@ data "vault_generic_secret" "ssh" {
   path = "digitalocean/ssh_key"
 }
 
+data "http" "personal_ssh_key" {
+  url = "https://api.github.com/users/brucellino/keys"
+}
 provider "digitalocean" {
   token             = data.vault_generic_secret.do_token.data["terraform"]
   spaces_access_id  = data.vault_generic_secret.do_token.data["spaces_key"]
@@ -60,4 +70,9 @@ resource "digitalocean_spaces_bucket" "images" {
 resource "digitalocean_ssh_key" "test_instance" {
   name       = var.ssh_key_name
   public_key = data.vault_generic_secret.ssh.data["public_key"]
+}
+
+resource "digitalocean_ssh_key" "personal" {
+  name       = "personal"
+  public_key = jsondecode(data.http.personal_ssh_key.body)[0].key
 }
